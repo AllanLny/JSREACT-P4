33085b607c42f9a5d983369d474bba1a1b19a7b1
Allan Lannoy <allan1411lannoy@gmail.com>
2023-11-05 22:01:17 +0100

    correction topnav

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,12 @@
+function editNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalContent = document.querySelector(".content")
